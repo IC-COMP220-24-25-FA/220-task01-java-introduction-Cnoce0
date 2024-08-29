270183350ca7c6443c693f92237f5b31217ab7f9
Cnoce0 <macnoce10@gmail.com>
2024-08-29 19:32:29 -0400

    tests

--- src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -15,4 +15,21 @@ public class RectangleTest {
         Rectangle rectangleTest3 = new Rectangle(5,1.5);
         assertEquals(7.5, rectangleTest3.calcArea(), .0001);
     }
+
+
+    public void longestLineWithinTest(){
+        Rectangle rectangleTest1 = new Rectangle(4,3);
+        assertEquals(5, rectangleTest1.longestLineWithin(), .0001);
+
+        Rectangle rectangleTest2 = new Rectangle(.4,.3);
+        assertEquals(.5, rectangleTest2.longestLineWithin(), .0001);
+
+        Rectangle rectangleTest3 = new Rectangle(6,7);
+        assertEquals(9.2195, rectangleTest3.longestLineWithin(), .0001);
+    }
+
+    public void doubleSizeTest(){
+        Rectangle rectangleTest1 = new Rectangle(4,3);
+        
+    }
 }
