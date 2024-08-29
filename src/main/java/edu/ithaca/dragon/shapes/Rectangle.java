1750737df604aef3ab2b1c2bcf30fd5d459f4532
Cnoce0 <macnoce10@gmail.com>
2024-08-29 19:40:12 -0400

    implemented some methods

--- src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
@@ -1,14 +1,17 @@
 package edu.ithaca.dragon.shapes;
 
 public class Rectangle {
-    
+    private double length;
+    private double width;
 
     public Rectangle(double length, double width){
-        throw new RuntimeException("Not implemented yet");
+         this.length = length;
+        this.width = width;
+
     }
 
     public double calcArea(){
-       throw new RuntimeException("Not implemented yet");
+       return this.length * this.width;
     }
 
     public void doubleSize(){
