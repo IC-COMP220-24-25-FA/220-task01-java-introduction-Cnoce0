cd760f5fc3c03fefc6e17b9c879b047fed1dc20f
Cnoce0 <macnoce10@gmail.com>
2024-08-29 19:18:37 -0400

    calc area of rectangle

---
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -0,0 +1,18 @@
+package edu.ithaca.dragon.shapes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RectangleTest {
+    
+
+    public void calcAreaTest(){
+        Rectangle rectangleTest = new Rectangle(4,2);
+        assertEquals(8, rectangleTest.calcArea(), .0001);
+
+        Rectangle rectangleTest2 = new Rectangle(2.5,1.5);
+        assertEquals(3.75, rectangleTest2.calcArea(), .0001);
+
+        Rectangle rectangleTest3 = new Rectangle(5,1.5);
+        assertEquals(7.5, rectangleTest3.calcArea(), .0001);
+    }
+}
