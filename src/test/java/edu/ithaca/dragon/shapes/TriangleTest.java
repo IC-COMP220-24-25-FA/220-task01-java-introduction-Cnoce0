054a87431d73d5f57bec100fe0b53eb1eb715162
Cnoce0 <macnoce10@gmail.com>
2024-09-01 17:03:04 -0400

    calc area triangle

--- src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -9,7 +9,13 @@ public class TriangleTest {
     @Test
     public void calacAreaTest(){
         Triangle triangletest1 = new Triangle(4,5);
-        
+        assertEquals(10, triangletest1.calcArea(), .0001);
+
+        Triangle triangletest2 = new Triangle(9,5);
+        assertEquals(22.5, triangletest2.calcArea(), .0001);
+
+        Triangle triangletest3 = new Triangle(.5,.5);
+        assertEquals(.125, triangletest3.calcArea(), .0001);
     }
 
     @Test
