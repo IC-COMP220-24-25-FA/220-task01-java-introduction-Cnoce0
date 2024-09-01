59c1e51e4b20bd766f630351ad59fee068e688f3
Cnoce0 <macnoce10@gmail.com>
2024-09-01 16:58:55 -0400

    added test file

---
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -0,0 +1,24 @@
+package edu.ithaca.dragon.shapes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TriangleTest {
+    
+    @Test
+    public void calacAreaTest(){
+        Triangle triangletest1 = new Triangle(4,5);
+        
+    }
+
+    @Test
+    public void longestLineWithinTest(){
+
+    }
+
+    @Test
+    public void doubleSizeTest(){
+
+    }
+}
